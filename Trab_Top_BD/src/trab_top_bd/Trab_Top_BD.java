fc24f2d94710329477752b5ff82acb85ba4c3bb1
ecanevelo <evercan2006@hotmail.com>
2015-07-17 00:53:44 -0300

    Mapa das Instituições
    Programa Java desenvolvimento com o intuito de criar uma ferramenta
    capaz de extrair conhecimento em bases de dados de instituições públicas
    de ensino, disponibilizadas em dados abertos pelo governo federal,
    produzindo um mapa de calor, utilizando o google maps.

---
+++ Trab_Top_BD/src/trab_top_bd/Trab_Top_BD.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trab_top_bd;
+
+/**
+ *
+ * @author Everton
+ */
+public class Trab_Top_BD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Tela_escolhe_opcoes().setVisible(true);
+            }
+        });
+    }
+    
+}
